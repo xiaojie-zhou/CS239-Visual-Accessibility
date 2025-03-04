a1eaa7a78cbc3a1960905cfddb9ffcd0a836beca
Xiaojie Zhou <46155366+xiaojie-zhou@users.noreply.github.com>
2025-03-04 00:07:40 -0800

    Update Uploader.vue

--- Prototype/frontend/src/components/user-input/Uploader.vue
+++ Prototype/frontend/src/components/user-input/Uploader.vue
@@ -10,7 +10,7 @@
     </div>
   </div>
   <div v-else class="upload-box">
-    <img :src="uploadedImage" alt="Uploaded Image" /> 
+    <img :src="uploadedImage" alt="Uploaded Image" />
     <div class="clear-btn-container">
       <ClearUploadBtn @clear="uploadedImage = null" />
     </div>
@@ -26,16 +26,16 @@ const file = ref(null);
 const uploadedImage = ref(null);
 
 const handleUpload = async (event) => {
-  file.value = event.target.files[0]; 
+  file.value = event.target.files[0];
   if (file.value) await uploadFile();
 };
 
 const handleDrop = async (event) => {
   event.preventDefault();
-  file.value = event.dataTransfer.files[0]; 
-  if (file.value) await uploadFile(); 
+  file.value = event.dataTransfer.files[0];
+  if (file.value) await uploadFile();
 };
- 
+
 const uploadFile = async () => {
   if (!file.value) return;
 
@@ -53,7 +53,7 @@ const uploadFile = async () => {
     alert("File uploaded successfully!");
 
     const imageToken = response.data.image;
-    const imageResponse = await axios.get(`http://127.0.0.1:5000/getImg?token=${imageToken}`, {
+    const imageResponse = await axios.get(`http://127.0.0.1:5000/get-preview?token=${imageToken}`, {
       responseType: "blob",
     });
     uploadedImage.value = URL.createObjectURL(imageResponse.data);
