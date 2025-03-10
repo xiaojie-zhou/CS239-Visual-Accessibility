f97c8ea52526184e2e3d0bd482acec7bfe5aace2
Xiaojie Zhou <46155366+xiaojie-zhou@users.noreply.github.com>
2025-03-09 21:31:04 -0700

    adding tool name to webpage

--- Prototype/frontend/src/components/Header.vue
+++ Prototype/frontend/src/components/Header.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="header-container">
-      <h1 class="header">Color Vision Diagram Converter</h1>
+      <h1 class="header">ColorSense: Enhancing Visual Clarity</h1>
       <hr class="line" />
     </div>
   </template>
-  
+
   <style scoped>
   .header-container {
     text-align: center;
     display: flex;
     flex-direction: column;
-    align-items: center; 
+    align-items: center;
   }
-  
+
   .header {
-    font-size: 80px;
+    font-size: 70px;
     margin-top: 50px;
     margin-bottom: 50px;
   }
-  
+
   .line {
     width: 100%;
     height: 2px;
@@ -27,4 +27,3 @@
     margin-bottom: 30px;
   }
   </style>
-  
