d10b99b20855b863b98d019ced58f022c8391d1e
Xiaojie Zhou <46155366+xiaojie-zhou@users.noreply.github.com>
2025-03-09 22:11:49 -0700

    Update app.py

--- Prototype/backend/app.py
+++ Prototype/backend/app.py
@@ -213,4 +213,5 @@ def get_score():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port)  # Bind to all network interfaces
