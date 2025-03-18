797c5a82592715b4487d3392556da3f4d613377f
Xiaojie Zhou <46155366+xiaojie-zhou@users.noreply.github.com>
2025-03-18 11:56:23 -0700

    gTag

--- Prototype/frontend/src/App.vue
+++ Prototype/frontend/src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
 import Header from '@/components/Header.vue'
 import Body from '@/components/Body.vue'
+import * as Cronitor from "@cronitorio/cronitor-rum";
+
+Cronitor.track("Pageview");
+this.$gtag.event()
 </script>
 
 <template>
@@ -8,7 +12,7 @@ import Body from '@/components/Body.vue'
     <Header />
     <Body />
   </div>
- 
+
 </template>
 
 <style scoped>
--- Prototype/frontend/src/main.js
+++ Prototype/frontend/src/main.js
@@ -2,11 +2,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import * as Cronitor from "@cronitorio/cronitor-rum";
+import VueGtagPlugin from "vue-gtag";
 
 
 const clientKey = import.meta.env.VITE_CRONITOR_CLIENT_KEY;
+const gTag = import.meta.env.VITE_GTAG_MEASUREMENT_ID;
+
 Cronitor.load(clientKey);
 
-createApp(App).mount('#app')
+const app = createApp(App);
 
-Cronitor.track("Pageview");
+app.use(VueGtagPlugin, {
+  config: { id: gTag }
+})
+
+app.mount('#app')
